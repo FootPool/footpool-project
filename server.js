dced42a15f9ee48c576110df368849299c4985b4
etubom <way2young4u@hotmail.com>
2018-07-31 19:27:32 +0100

    Added working links to  handlebars

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 app.use(bodyParser.json());
-app.use('/static', express.static('static'));
-app.set('view engine', 'hbs');
+app.use("/static", express.static("static"));
+app.set("view engine", "hbs");
 
-app.get('/', function(req, res){
-  res.render('reset');
-})
+app.get("/", function(req, res) {
+  res.render("homepage");
+});
 
-app.listen(8080, function(){
-  console.log('Listening on port 8080');
+app.get("/signup", function(req, res) {
+  res.render("signup");
+});
+
+app.get("/login", function(req, res) {
+  res.render("login");
+});
+
+app.get("/reset", function(req, res) {
+  res.render("reset");
+});
+
+app.listen(8080, function() {
+  console.log("Listening on port 8080");
 });
