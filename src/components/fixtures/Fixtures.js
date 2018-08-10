0d9d3ddf5029efce1d59b732d8ac3af935fd75ea
Ollie Crook <olliejcrook@gmail.com>
2018-08-10 15:49:20 +0100

    update redux changes to merge

--- src/components/fixtures/Fixtures.js
+++ src/components/fixtures/Fixtures.js
@@ -17,7 +17,6 @@ class Fixtures extends React.Component {
   }
 
   render() {
-    console.log(this.props.matches);
     return (
       <div className="fixtures--container">
         <Header title="Fixtures" />
